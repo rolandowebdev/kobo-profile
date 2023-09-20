4e2fdd5674dd9e9ccb80a29cb0e791309b6fd164
rolandowebdev <rolandopranata@gmail.com>
2023-09-20 21:05:37 +0700

    feat: create music section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,65 @@
 const blur = document.querySelector('.blur')
 const hamburger = document.querySelector('.hamburger')
 const mobileMenuContainer = document.querySelector('.mobile-menu-container')
+const swiperWrapper = document.getElementById('swiper-wrapper')
 
 const SKINS_NAME = {
 	ORIGINAl: 'ORIGINAL',
 	KIMONO: 'KIMONO',
 	BATIK: 'BATIK'
 }
+
+const musicList = [
+	{
+		id: 1,
+		title: 'Mantra Hujan',
+		releaseDate: '2022-03-07',
+		category: ['Single', 'Original'],
+		coverImage: '../assets/music/mantra-hujan.jpeg'
+	},
+	{
+		id: 2,
+		title: 'Oh! Asmara',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Original'],
+		coverImage: '../assets/music/oh-asmara.jpeg'
+	},
+	{
+		id: 3,
+		title: 'Overdose',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/overdose.jpg'
+	},
+	{
+		id: 4,
+		title: 'Tondemo Wonder',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/tondemo-wonder.jpg'
+	},
+	{
+		id: 5,
+		title: 'Happy',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/happy.jpg'
+	},
+	{
+		id: 6,
+		title: 'Princess Samurai',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/princess-samurai.jpg'
+	},
+	{
+		id: 7,
+		title: 'Everything Goes On',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/everything-goes-on.jpg'
+	}
+]
 
 const switchSkins = (skinName) => {
 	const image = document.getElementById('skins-image')
@@ -29,10 +82,41 @@ const switchSkins = (skinName) => {
 				console.log('Skin not found!')
 				break
 		}
-
 		skinsWrapper.style.opacity = 1
 	}, 300)
 }
+
+const translateDate = (inputDateStr) => {
+	return new Date(inputDateStr).toLocaleDateString('en-GB', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric'
+	})
+}
+
+swiperWrapper.innerHTML = musicList
+	.map(
+		(music) =>
+			`<div class="swiper-slide music-slider-item" lazy="true">
+				<img
+					loading="lazy"
+					class="music-background-image"
+					src="${music.coverImage}"
+					alt="${music.title}" />
+				<div class="music-description">
+					<h3 class="music-title">${music.title}</h3>
+					<time class="music-release-date" datetime="${music.releaseDate}"
+						>${translateDate(music.releaseDate)}</time
+					>
+				</div>
+				<div class="music-category">
+					${music.category
+						.map((category) => `<p class="music-category-item">${category}</p>`)
+						.join('')}
+				</div>
+			</div>`
+	)
+	.join('')
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active')
