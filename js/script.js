8e45b0c4248445bc67f37184481ced9e80655d7f
rolandowebdev <rolandopranata@gmail.com>
2023-09-20 22:33:27 +0700

    feat: add button next and prev swiper

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const blur = document.querySelector('.blur')
 const hamburger = document.querySelector('.hamburger')
 const mobileMenuContainer = document.querySelector('.mobile-menu-container')
+const image = document.getElementById('skins-image')
+const skinsWrapper = document.querySelector('.skins-image-wrapper')
 const swiperWrapper = document.getElementById('swiper-wrapper')
+const newReleasedContainer = document.getElementById('new-released-container')
 
 const SKINS_NAME = {
 	ORIGINAl: 'ORIGINAL',
@@ -9,64 +12,70 @@ const SKINS_NAME = {
 	BATIK: 'BATIK'
 }
 
+const newReleasedMusic = [
+	{
+		id: 1,
+		title: 'Everything Goes On',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/everything-goes-on.jpg',
+		link: 'https://www.youtube.com/watch?v=YQw3lhyJMzo'
+	},
+	{
+		id: 2,
+		title: 'Princess Samurai',
+		releaseDate: '2023-03-07',
+		category: ['Single', 'Cover'],
+		coverImage: '../assets/music/princess-samurai.jpg',
+		link: 'https://www.youtube.com/watch?v=jkgaIFbdlpE'
+	}
+]
+
 const musicList = [
 	{
 		id: 1,
 		title: 'Mantra Hujan',
 		releaseDate: '2022-03-07',
 		category: ['Single', 'Original'],
-		coverImage: '../assets/music/mantra-hujan.jpeg'
+		coverImage: '../assets/music/mantra-hujan.jpeg',
+		link: 'https://www.youtube.com/watch?v=SF-_47-oCtk'
 	},
 	{
 		id: 2,
 		title: 'Oh! Asmara',
 		releaseDate: '2023-03-07',
 		category: ['Single', 'Original'],
-		coverImage: '../assets/music/oh-asmara.jpeg'
+		coverImage: '../assets/music/oh-asmara.jpeg',
+		link: 'https://www.youtube.com/watch?v=DR43SQx8Ybc'
 	},
 	{
 		id: 3,
 		title: 'Overdose',
 		releaseDate: '2023-03-07',
 		category: ['Single', 'Cover'],
-		coverImage: '../assets/music/overdose.jpg'
+		coverImage: '../assets/music/overdose.jpg',
+		link: 'https://www.youtube.com/watch?v=iiw9Z1I1AcE'
 	},
 	{
 		id: 4,
 		title: 'Tondemo Wonder',
 		releaseDate: '2023-03-07',
 		category: ['Single', 'Cover'],
-		coverImage: '../assets/music/tondemo-wonder.jpg'
+		coverImage: '../assets/music/tondemo-wonder.jpg',
+		link: 'https://www.youtube.com/watch?v=8fLie0tmhcA'
 	},
 	{
 		id: 5,
 		title: 'Happy',
 		releaseDate: '2023-03-07',
 		category: ['Single', 'Cover'],
-		coverImage: '../assets/music/happy.jpg'
-	},
-	{
-		id: 6,
-		title: 'Princess Samurai',
-		releaseDate: '2023-03-07',
-		category: ['Single', 'Cover'],
-		coverImage: '../assets/music/princess-samurai.jpg'
-	},
-	{
-		id: 7,
-		title: 'Everything Goes On',
-		releaseDate: '2023-03-07',
-		category: ['Single', 'Cover'],
-		coverImage: '../assets/music/everything-goes-on.jpg'
+		coverImage: '../assets/music/happy.jpg',
+		link: 'https://www.youtube.com/watch?v=MK78JU18SxE'
 	}
 ]
 
 const switchSkins = (skinName) => {
-	const image = document.getElementById('skins-image')
-	const skinsWrapper = document.querySelector('.skins-image-wrapper')
-
 	skinsWrapper.style.opacity = 0
-
 	setTimeout(() => {
 		switch (skinName) {
 			case SKINS_NAME.ORIGINAl:
@@ -94,6 +103,29 @@ const translateDate = (inputDateStr) => {
 	})
 }
 
+newReleasedContainer.innerHTML = newReleasedMusic
+	.map(
+		(music) =>
+			`<div class="music-slider-item">
+				<img
+					class="music-background-image"
+					src="${music.coverImage}"
+					alt="${music.title}" />
+				<div class="music-description">
+					<a href="${music.link}" class="music-title" target="_blank">${music.title}</a>
+					<time class="music-release-date" datetime="${music.releaseDate}"
+						>${translateDate(music.releaseDate)}</time
+					>
+				</div>
+				<div class="music-category">
+					${music.category
+						.map((category) => `<p class="music-category-item">${category}</p>`)
+						.join('')}
+				</div>
+			</div>`
+	)
+	.join('')
+
 swiperWrapper.innerHTML = musicList
 	.map(
 		(music) =>
@@ -104,7 +136,7 @@ swiperWrapper.innerHTML = musicList
 					src="${music.coverImage}"
 					alt="${music.title}" />
 				<div class="music-description">
-					<h3 class="music-title">${music.title}</h3>
+					<a href="${music.link}" class="music-title" target="_blank">${music.title}</a>
 					<time class="music-release-date" datetime="${music.releaseDate}"
 						>${translateDate(music.releaseDate)}</time
 					>
