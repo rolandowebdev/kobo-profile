b3fc46296d102afdadb3c18ef44b6edc98a859d2
rolandowebdev <rolandopranata@gmail.com>
2023-09-22 20:23:09 +0700

    feat: create videos section

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ const hamburger = document.querySelector('.hamburger')
 const mobileMenuContainer = document.querySelector('.mobile-menu-container')
 const image = document.getElementById('skins-image')
 const skinsWrapper = document.querySelector('.skins-image-wrapper')
-const swiperWrapper = document.getElementById('swiper-wrapper')
+const musicListWrapper = document.getElementById('music-list-wrapper')
+const videoListWrapper = document.getElementById('video-list-wrapper')
 const newReleasedContainer = document.getElementById('new-released-container')
+const highlightsVideoContainer = document.getElementById(
+	'highlights-video-container'
+)
+const notificationContainer = document.getElementById('notification-container')
 
 const SKINS_NAME = {
 	ORIGINAl: 'ORIGINAL',
@@ -74,6 +79,109 @@ const musicList = [
 	}
 ]
 
+const highlightVideo = [
+	{
+		id: 1,
+		title: 'Holo ID Cup 2023',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Collab'],
+		coverImage: '../assets/videos/holoid-cup-live.jpg',
+		link: 'https://www.youtube.com/live/eOsyI2qgWL8?si=rSptIySRT87PDEb4'
+	}
+]
+
+const listVideo = [
+	{
+		id: 1,
+		title: 'Amnesia Live',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Game', 'Horror'],
+		coverImage: '../assets/videos/amnesia-live.jpg',
+		link: 'https://www.youtube.com/live/8POOuQNv9i8?si=9GzwFRkd0kBNExnm'
+	},
+	{
+		id: 2,
+		title: 'Cooking Simulator',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Game', 'Cooking'],
+		coverImage: '../assets/videos/cooking-simulator-live.jpg',
+		link: 'https://www.youtube.com/live/3VzV-7Vdzy4?si=QYCnlOeEyFq8wKU6'
+	},
+	{
+		id: 3,
+		title: 'Freetalk',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Freetalk'],
+		coverImage: '../assets/videos/freetalk-live.jpg',
+		link: 'https://www.youtube.com/live/V70iQXIuVtw?si=Lxr0DORdLv0O4YkM'
+	},
+	{
+		id: 4,
+		title: 'Holocure',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Game'],
+		coverImage: '../assets/videos/holocure-live.jpg',
+		link: 'https://www.youtube.com/live/sEBAGq0cd58?si=Fugo99t8QG_0YoF9'
+	},
+	{
+		id: 5,
+		title: 'Parasocial',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Game', 'Horror'],
+		coverImage: '../assets/videos/parasocial-live.jpg',
+		link: 'https://www.youtube.com/live/PfFQ7AX5PNI?si=5pY6quujorqWHl0A'
+	},
+	{
+		id: 6,
+		title: 'Pokemon Unite',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Game', 'Moba'],
+		coverImage: '../assets/videos/pokemon-unite-live.jpg',
+		link: 'https://www.youtube.com/live/37MjcI5CSdY?si=M-KU7FkybHZ3-jaG'
+	},
+	{
+		id: 7,
+		title: 'Valorant',
+		releaseDate: '2023-03-07',
+		category: ['Live', 'Game', 'FPS'],
+		coverImage: '../assets/videos/valorant-live.jpg',
+		link: 'https://www.youtube.com/live/WjdWxSZynAo?si=A4TAtPOGFiHDlL_t'
+	}
+]
+
+const listNotification = [
+	{
+		id: 1,
+		title: 'Chat room',
+		description: 'Free talk & Donation reading',
+		coverImage: '../assets/videos/chat-room.jpg',
+		date: 'Tod, 17:15'
+	},
+	{
+		id: 2,
+		title: 'Post Talk',
+		description: 'Talking about my new costume & donation reading',
+		coverImage: '../assets/videos/post-talk.jpg',
+		date: 'Tod, 20:15'
+	},
+	{
+		id: 3,
+		title: 'New Costume Relay',
+		description:
+			'REVEAL SKIN BARU KOBO!!! JADI WAYANG? JADI PUTRI RAJA?? JADI-JADIAN???',
+		coverImage: '../assets/videos/new-costume-relay.jpg',
+		date: 'Tom, 15:30'
+	},
+	{
+		id: 4,
+		title: 'New Costume Relay',
+		description:
+			'REVEAL SKIN BARU KOBO!!! JADI WAYANG? JADI PUTRI RAJA?? JADI-JADIAN???',
+		coverImage: '../assets/videos/new-costume-relay.jpg',
+		date: 'Tom, 15:30'
+	}
+]
+
 const switchSkins = (skinName) => {
 	skinsWrapper.style.opacity = 0
 	setTimeout(() => {
@@ -106,47 +214,117 @@ const translateDate = (inputDateStr) => {
 newReleasedContainer.innerHTML = newReleasedMusic
 	.map(
 		(music) =>
-			`<div class="music-slider-item">
+			`<div class="card-item">
 				<img
-					class="music-background-image"
+					class="card-background-image"
 					src="${music.coverImage}"
 					alt="${music.title}" />
-				<div class="music-description">
-					<a href="${music.link}" class="music-title" target="_blank">${music.title}</a>
-					<time class="music-release-date" datetime="${music.releaseDate}"
+				<div class="card-description">
+					<a href="${music.link}" class="card-title" target="_blank">${music.title}</a>
+					<time class="card-release-date" datetime="${music.releaseDate}"
 						>${translateDate(music.releaseDate)}</time
 					>
 				</div>
-				<div class="music-category">
+				<div class="card-category">
 					${music.category
-						.map((category) => `<p class="music-category-item">${category}</p>`)
+						.map((category) => `<p class="card-category-item">${category}</p>`)
 						.join('')}
 				</div>
 			</div>`
 	)
 	.join('')
 
-swiperWrapper.innerHTML = musicList
+musicListWrapper.innerHTML = musicList
 	.map(
 		(music) =>
-			`<div class="swiper-slide music-slider-item" lazy="true">
+			`<div class="swiper-slide card-item" lazy="true">
 				<img
 					loading="lazy"
-					class="music-background-image"
+					class="card-background-image"
 					src="${music.coverImage}"
 					alt="${music.title}" />
-				<div class="music-description">
-					<a href="${music.link}" class="music-title" target="_blank">${music.title}</a>
-					<time class="music-release-date" datetime="${music.releaseDate}"
+				<div class="card-description">
+					<a href="${music.link}" class="card-title" target="_blank">${music.title}</a>
+					<time class="card-release-date" datetime="${music.releaseDate}"
 						>${translateDate(music.releaseDate)}</time
 					>
 				</div>
-				<div class="music-category">
+				<div class="card-category">
 					${music.category
-						.map((category) => `<p class="music-category-item">${category}</p>`)
+						.map((category) => `<p class="card-category-item">${category}</p>`)
 						.join('')}
 				</div>
 			</div>`
+	)
+	.join('')
+
+highlightsVideoContainer.innerHTML = highlightVideo
+	.map(
+		(video) =>
+			`<div class="card-item">
+				<img
+					class="card-background-image"
+					src="${video.coverImage}"
+					alt="${video.title}" />
+				<div class="card-description">
+					<a href="${video.link}" class="card-title" target="_blank">${video.title}</a>
+					<time class="card-release-date" datetime="${video.releaseDate}"
+						>${translateDate(video.releaseDate)}</time
+					>
+				</div>
+				<div class="card-category">
+					${video.category
+						.map((category) => `<p class="card-category-item">${category}</p>`)
+						.join('')}
+				</div>
+			</div>`
+	)
+	.join('')
+
+videoListWrapper.innerHTML = listVideo
+	.map(
+		(video) =>
+			`<div class="swiper-slide card-item" lazy="true">
+				<img
+				loading="lazy"
+					class="card-background-image"
+					src="${video.coverImage}"
+					alt="${video.title}" />
+				<div class="card-description">
+					<a href="${video.link}" class="card-title" target="_blank">${video.title}</a>
+					<time class="card-release-date" datetime="${video.releaseDate}"
+						>${translateDate(video.releaseDate)}</time
+					>
+				</div>
+				<div class="card-category">
+					${video.category
+						.map((category) => `<p class="card-category-item">${category}</p>`)
+						.join('')}
+				</div>
+			</div>`
+	)
+	.join('')
+
+notificationContainer.innerHTML = listNotification
+	.map(
+		(notification) =>
+			`<li class="notification-item">
+			<div class="notification-image">
+				<img src="${notification.coverImage}" alt="${notification.title}" />
+			</div>
+
+			<div class="notification-text">
+				<div class="notification-text-wrapper">
+					<a href="#" target="_blank" class="notification-link"
+						>${notification.title}</a
+					>
+					<time datetime="${notification.date}">${notification.date}</time>
+				</div>
+				<p class="notification-description">
+					${notification.description}
+				</p>
+			</div>
+		</li>`
 	)
 	.join('')
 
